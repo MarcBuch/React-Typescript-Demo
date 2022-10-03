67e85359292d8e459749fc55471b2a8cb34f1a9e
Marc Buchardt <marc@buchardtprivat.de>
2022-10-03 12:05:13 +0200

    feat(style): add system dark mode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -45,10 +45,10 @@ const Home: NextPage = () => {
   const { todos, text, updateStatus, addTodo, setText } = useTodos();
 
   return (
-    <div className="py-4 min-h-screen flex flex-col">
+    <div className="py-4 min-h-screen flex flex-col items-center dark:bg-slate-900 dark:text-white">
       <Head>
         <title>ToDo App</title>
-        <meta name="description" content="My ToDo App" />
+        <meta name="description" content="ToDo App" />
       </Head>
       <main className="flex flex-col flex-1 items-center">
         <h1 className="m-2 text-4xl font-bold">ToDo List</h1>
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
         </ul>
       </main>
 
-      <footer className="mt-auto flex flex-col items-center">
+      <footer className="mt-auto w-full max-w-xl flex flex-row justify-around">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -100,6 +100,22 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        </button>
       </footer>
     </div>
   );
