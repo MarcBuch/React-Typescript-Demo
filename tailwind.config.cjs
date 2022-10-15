cabb285a1a69a07fcc3bd62ed63c0e6cd92c9789
Marc Buchardt <marc@buchardtprivat.de>
2022-10-15 15:30:43 +0200

    feat(styling): add toggle for dark mode

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        spaceGrotesk: ['Space Grotesk', 'sans-serif'],
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
       },
     },
   },
